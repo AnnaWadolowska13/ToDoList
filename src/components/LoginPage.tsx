c9e8d61303de46cf780b54ca293fa4fff6ce5a8d
Anna Wadolowska <annaklej1996@gmail.com>
2022-04-02 10:19:56 +0200

    add loginPage component

---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LogForm from "./LogForm";
+import Welcome from "./Welcome";
+
+function LoginPage() {
+    return (
+        <>
+            <Welcome/>
+            <LogForm/>
+        </>
+    )
+}
+
+export default LoginPage;
